90cf308e8690d8a5378ac8fe355984651dd6344d
Sladki <11015154+Sladki@users.noreply.github.com>
2025-09-07 21:12:50 +1000

    Potential inlining and 2.7 compatibility fix

--- src/main/java/com/github/sladki/gtnhrates/mixins/late/GTMetaTools.java
+++ src/main/java/com/github/sladki/gtnhrates/mixins/late/GTMetaTools.java
@@ -1,20 +1,28 @@
 package com.github.sladki.gtnhrates.mixins.late;
 
+import net.minecraft.item.ItemStack;
+
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import com.github.sladki.gtnhrates.ModConfig;
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
 import gregtech.api.items.MetaGeneratedTool;
 
 @Mixin(value = MetaGeneratedTool.class, remap = false)
 public abstract class GTMetaTools {
 
-    @ModifyExpressionValue(
-        method = "getContainerItem(Lnet/minecraft/item/ItemStack;)Lnet/minecraft/item/ItemStack;",
-        at = @At(value = "INVOKE", target = "Lgregtech/api/interfaces/IToolStats;getToolDamagePerContainerCraft()I"))
-    private int onGetToolDamagePerContainerCraft(int original) {
-        return (int) Math.ceil(original / ModConfig.Rates.gtToolsCraftingDurability);
+    @Shadow
+    public abstract boolean doDamage(ItemStack aStack, long aAmount);
+
+    @Redirect(
+        method = "getContainerItem*",
+        at = @At(
+            value = "INVOKE",
+            target = "Lgregtech/api/items/MetaGeneratedTool;doDamage(Lnet/minecraft/item/ItemStack;J)Z"))
+    private boolean onDamagingCraftingTool(MetaGeneratedTool instance, ItemStack tNewDamage, long tStats) {
+        return doDamage(tNewDamage, (int) Math.ceil(tStats / ModConfig.Rates.gtToolsCraftingDurability));
     }
 }
